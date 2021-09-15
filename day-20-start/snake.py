c34eb7fbaa60bb5d9ebdf7f3e9c0a521f09f32c1
Coreywilson319 <coreywilson319@gmail.com>
2021-09-15 07:48:05 -0400

    day 20 finish

---
+++ day-20-start/snake.py
@@ -0,0 +1,48 @@
+from turtle import Turtle
+
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+    def __init__(self):
+        self.snake_body = []
+        self.create_snake()
+        self.head = self.snake_body[0]
+
+    def create_snake(self):
+        for i in range(3):
+            new_segment = Turtle(shape="square")
+            new_segment.color("white")
+            new_segment.penup()
+            self.snake_body.append(new_segment)
+        for segment in range(len(self.snake_body)):
+            self.snake_body[segment].setposition(x=segment * -20, y=0)
+
+    def move(self):
+        for seg_num in range(len(self.snake_body) - 1, 0, -1):
+            new_x = self.snake_body[seg_num - 1].xcor()
+            new_y = self.snake_body[seg_num - 1].ycor()
+            self.snake_body[seg_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+
