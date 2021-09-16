2466f3f0d7d0295796c3de2236a8ee301f936806
Coreywilson319 <coreywilson319@gmail.com>
2021-09-16 14:30:03 -0400

    day 24

--- day-24/main.py
+++ day-24/main.py
@@ -1,2 +1,16 @@
-with open("new_file.txt", mode="w") as file:
-    file.write("New Text")
+#TODO: Create a letter using starting_letter.txt 
+#for each name in invited_names.txt
+#Replace the [name] placeholder with the actual name.
+#Save the letters in the folder "ReadyToSend".
+PLACEHOLDER = "[name]"
+
+with open('Input/Names/invited_names.txt') as names:
+    names = names.readlines()
+
+with open('Input/Letters/starting_letter.txt') as letter:
+    letter = letter.read()
+    for name in names:
+        stripped_name = name.strip()
+        with open(f'Output/ReadyToSend/letter_for_{name}', mode="w") as ready_to_send_letter:
+            modified_letter = letter.replace(PLACEHOLDER, stripped_name)
+            ready_to_send_letter.write(modified_letter)
